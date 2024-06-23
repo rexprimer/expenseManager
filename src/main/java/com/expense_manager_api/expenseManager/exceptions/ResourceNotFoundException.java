fb14f369938c7fc359c7b9839d9f5bdf305bdeee
vikashchaurasia <vikash_chaurasia@outlook.com>
2024-06-23 15:10:05 +0530

    Added the User

---
+++ src/main/java/com/expense_manager_api/expenseManager/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,2 @@
+package com.expense_manager_api.expenseManager.exceptions;public class ResourceNotFoundException {
+}
---
+++ src/main/java/com/expense_manager_api/expenseManager/service/UserServiceImpl.java
@@ -0,0 +1,2 @@
+package com.expense_manager_api.expenseManager.service;public class UserServiceImpl {
+}
